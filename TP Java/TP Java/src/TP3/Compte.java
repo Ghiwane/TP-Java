07d217034b9eb4295189e8fe1d0e30a5983bbee2
Ghiwane <sehaki.ghiwane@gmail.com>
2026-09-17 11:39:30 +0200

    correctif du constructeur de la classe Compte (nom corrigé)

--- TP Java/TP Java/src/TP3/Compte.java
+++ TP Java/TP Java/src/TP3/Compte.java
@@ -8,12 +8,12 @@ public class Compte {
     private int solde;
     private boolean connected;
 
-    public void Compte(){
+    public Compte(){
         identifiant = "anonyme";
         dateDeCreation = "17/09/2026";
         actif = true;
     }
-    public void Compte(String identifiant, String dateDeCreation){
+    public Compte(String identifiant, String dateDeCreation){
         this.identifiant = identifiant;
         this.dateDeCreation = dateDeCreation;
         this.actif = true;
