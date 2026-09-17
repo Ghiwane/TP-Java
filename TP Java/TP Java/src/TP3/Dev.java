74c09ba01ff3d7df1f90b747841374777142e8e7
Ghiwane <sehaki.ghiwane@gmail.com>
2026-09-17 09:44:30 +0200

    ajout des attributs et méthodes pour les classes Dev et Jeu

--- TP Java/TP Java/src/TP3/Dev.java
+++ TP Java/TP Java/src/TP3/Dev.java
@@ -1,5 +1,15 @@
 package TP3;
 
 public class Dev extends Utilisateur {
+    private String nomStudio;
+    private String specialite;
+    private int nbJeuPublie;
 
+    public Dev(String pseudo, String email, String mdp, String nomStudio, String specialite, int nbJeuPublie)
+    {
+        super(pseudo, email, mdp);
+        this.nomStudio = nomStudio;
+        this.specialite = specialite;
+        this.nbJeuPublie = nbJeuPublie;
+    }
 }
--- TP Java/TP Java/src/TP3/Jeu.java
+++ TP Java/TP Java/src/TP3/Jeu.java
@@ -1,4 +1,38 @@
 package TP3;
 
 public class Jeu {
+    private String titre;
+    double prix;
+    private String genre;
+
+    public void Jeu(String titre, double prix, String genre)
+    {
+        this.titre = titre;
+        this.prix = prix;
+        this.genre = genre;
+    }
+
+    public void affiche()
+    {
+        System.out.println("Titre : " + titre);
+        System.out.println("Prix : " + prix);
+        System.out.println("Genre : " + genre);
+    }
+
+    public void modifierPrix(double prix)
+    {
+        this.prix = prix;
+    }
+
+    public void estDisponible(boolean disponible)
+    {
+        if (disponible)
+        {
+            System.out.println("Le jeu est disponible");
+        }
+        else
+        {
+            System.out.println("Le jeu n'est pas disponible");
+        }
+    }
 }
